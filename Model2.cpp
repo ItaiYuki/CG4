43e8f79abcc02355cfd39fa0e0f344f8b8dd4e12
ItaiYuki <K024G0055@g.neec.ac.jp>
2026-05-08 11:00:38 +0900

    完了

--- Model2.cpp
+++ Model2.cpp
@@ -831,19 +831,19 @@ Model2* Model2::CreateSquare(int max) {
 	for (int i = 0; i < max; i++) {
 		int index = i * 4;
 		// 左下
-		vertices[index + 0].pos = {i * 2 + -2.0f, 6.0f, 0.0f};
+		vertices[index + 0].pos = {i * 4 + -10.0f, -1.0f, 0.0f};
 		vertices[index + 0].uv = {0, 1};
 		vertices[index + 0].normal = {0, 0, 1};
 		// 左上
-		vertices[index + 1].pos = {i * 2 + -2.0f, 10.0f, 0.0f};
+		vertices[index + 1].pos = {i * 4 + -10.0f, 3.0f, 0.0f};
 		vertices[index + 1].uv = {0, 0};
 		vertices[index + 1].normal = {0, 0, 1};
 		// 右下
-		vertices[index + 2].pos = {i * 2 + 2.0f, 6.0f, 0.0f};
+		vertices[index + 2].pos = {i * 4 + -6.0f, -1.0f, 0.0f};
 		vertices[index + 2].uv = {1, 1};
 		vertices[index + 2].normal = {0, 0, 1};
 		// 右上
-		vertices[index + 3].pos = {i * 2 + 2.0f, 10.0f, 0.0f};
+		vertices[index + 3].pos = {i * 4 + -6.0f, 3.0f, 0.0f};
 		vertices[index + 3].uv = {1, 0};
 		vertices[index + 3].normal = {0, 0, 1};
 	}
